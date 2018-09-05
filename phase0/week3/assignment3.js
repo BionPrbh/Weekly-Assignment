befef7e1914eae0bf74447d969cd861cb27c7237
Cascade <mufhabu@gmail.com>
2018-09-05 16:04:08 +0700

    re-upload assignment3.js

--- phase0/week3/assignment3.js
+++ phase0/week3/assignment3.js
@@ -1,34 +1,16 @@
 
-
 function dataHandling(input){
 
-  var nomorID= "" 
-  var namaLengkap = "" 
-  var tempatTanggalLahir = "" 
-  var hobi = "" 
 
-  indexOfData = 0
-  while(indexOfData < input.length){
-
-    for(indexOfDataDetails = 0; indexOfDataDetails < input[0].length-1;indexOfDataDetails += 1){
-      if(indexOfDataDetails == 0){
-        nomorID = "Nomor ID:  " + input[indexOfData][indexOfDataDetails]
-      }else if(indexOfDataDetails == 1){
-        namaLengkap = "Nama Lengkap:  " + input[indexOfData][indexOfDataDetails]
-      }else if(indexOfDataDetails == 2){
-        tempatTanggalLahir = "TTL:  " + input[indexOfData][indexOfDataDetails]
-      }else if(indexOfDataDetails == 3){
-        hobi = "Hobi:  " + input[indexOfData][indexOfDataDetails]
-      }
-        
-    }
-
-    console.log(nomorID + "\n" + namaLengkap + "\n" + tempatTanggalLahir + "\n" +hobi + "\n")
-    indexOfData += 1
+  for (i=0; i < input.length; i++){
+	  console.log("Nomor ID:  "+input[i][0])
+    console.log("Nama Lengkap:  "+input[i][1])
+    console.log("TTL:  "+input[i][2]+" "+input[0][3])
+    console.log("Hobi:  "+input[i][4] + "\n")
   }
+
+  return
 }
-   
-  
 
 
 dataHandling([
@@ -37,27 +19,3 @@ dataHandling([
   ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
   ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
 ])
-
-/*
-
-0 0
-0 1
-0 2
-0 3
-
-1 0
-1 1
-1 2
-1 3
-
-2 0
-2 1
-2 2
-2 3
-
-3 0
-3 1
-3 2
-3 3
-
-*/
