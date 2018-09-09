bee6f7a5191e30d937739813071553b5ddca5cbf
Cascade <mufhabu@gmail.com>
2018-09-09 22:02:23 +0700

    add revision on week 2: exercise 2 and 4

--- phase0week2/exercise4.js
+++ phase0week2/exercise4.js
@@ -1,161 +1,3 @@
-SOAL KEDUA cara 1
-
-var tanggal = 2;
-var bulan = 8;
-var tahun = 1994;
-
-switch (tanggal){
-  case 1;
-   console.log(1);
-   break;
-  case 2;
-   console.log(2);
-   break;
-  case 3;
-   console.log(3);
-   break;
-  case 4;
-   console.log(4);
-   break;
-  case 5;
-   console.log(5);
-   break;
-  case 6;
-   console.log(6);
-   break;
-  case 7;
-   console.log(7);
-   break;
-  case 8;
-   console.log(8);
-   break;
-  case 9;
-   console.log(9);
-   break;
-  case 10;
-   console.log(10);
-   break;
-  case 11;
-   console.log(11);
-   break;
-  case 12;
-   console.log(12);
-   break;
-  case 13;
-   console.log(13);
-   break;
-  case 14;
-   console.log(14);
-   break;
-  case 15;
-   console.log(15);
-   break;
-  case 16;
-   console.log(16);
-   break;
-  case 17;
-   console.log(17);
-   break;
-  case 18;
-   console.log(18).
-   break;
-  case 19;
-   console.log(19);
-   break;
-  case 20;
-   console.log(20);
-   break;
-  case 21;
-   console.log(21);
-   break;
-  case 22;
-   console.log(22);
-   break;
-  case 23;
-   console.log(23);
-   break;
-  case 24;
-   console.log(24);
-   break;
-  case 25;
-   console.log(25);
-   break;
-  case 26;
-   console.log(26);
-   break;
-  case 27;
-   console.log(27);
-   break;
-  case 28;
-   console.log(28);
-   break;
-  case 29;
-   console.log(29);
-   break;
-  case 30;
-   console.log(30);
-   break;
-  case 31;
-   console.log(31);
-   break;
-  default:
-   console.log("masukkan tanggal");
-   break;
-}
-switch (bulan){
-  case 1;
-   console.log("Januari");
-   break;
-  case 2;
-   console.log("Februari");
-   break;
-  case 3;
-   console.log("Maret");
-   break;
-  case 4;
-   console.log("April");
-   break;
-  case 5;
-   console.log("Mei");
-   break;
-  case 6;
-   console.log("Juni");
-   break;
-  case 7;
-   console.log("Juli");
-   break;
-  case 8;
-   console.log("Agustus");
-   break;
-  case 9;
-   console.log("September");
-   break;
-  case 10;
-   console.log("Oktober");
-   break;
-  case 11;
-   console.log("November");
-   break;
-  case 12;
-   console.log("Desember");
-   break;
-  default:
-   console.log("masukkan bulan");
-   break;
-}
-switch (tahun){
-  console.log(tahun)
-  default:
-   console.log("masukkan tahun");
-   break;
-}
-
-
-
-
-
-SOAL KEDUA cara 2
-
 var tanggal = 1;
 var bulan = 11;
 var tahun = 1995;
@@ -167,31 +9,31 @@ if (tahun > 0 && tahun < 2018){
   tahun = tahun;
 }
 switch (bulan){
-  case 01: 
+  case 1: 
    console.log(tanggal + " Januari " + tahun);
    break;
-  case 02:
+  case 2:
    console.log(tanggal + " Februari " + tahun);
    break;
-  case 03:
+  case 3:
    console.log(tanggal + " Maret " + tahun);
    break;
-  case 04:
+  case 4:
    console.log(tanggal + " April " + tahun);
    break;
-  case 05:
+  case 5:
    console.log(tanggal + " Mei " + tahun);
    break;
-  case 06:
+  case 6:
    console.log(tanggal + " Juni " + tahun);
    break;
-  case 07:
+  case 7:
    console.log(tanggal + " Juli " + tahun);
    break;
-  case 08:
+  case 8:
    console.log(tanggal + " Agustus " + tahun);
    break;
-  case 09:
+  case 9:
    console.log(tanggal + " September " + tahun);
    break;
   case 10:
